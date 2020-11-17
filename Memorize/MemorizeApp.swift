ccc34a22cf0bb712ce172e61724f7445af8110ab
lyrisli <lyrisli@tencent.com>
2020-11-17 10:19:42 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 李宜嵘 on 2020/11/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
